525b304cd67da5cc48d59fdadc5c916e2c3c9569
sinabeuro <ican312@hanmail.net>
2016-01-09 00:50:15 +0900

    [m] 리스트뷰 추가, 폴더단위 기반

--- WpfApplication1/WpfApplication1.py
+++ WpfApplication1/WpfApplication1.py
@@ -20,7 +20,7 @@ class MyWindow(Window):
 
     def traverseDir(self, root):
         for dirName, subdirList, fileList in os.walk(root):
-            mov = next((x for x in fileList if any(os.path.splitext(x)[1] == mov_type for mov_type in mov_types)), None)
+            mov = next((x for x in fileList if any(os.path.splitext(x)[1] == mov_type for mov_type in mov_types)), None) # 2 and more?
             self.mov.Text = mov
             if mov is None:
                 continue
@@ -28,15 +28,15 @@ class MyWindow(Window):
                  continue
             #ifilter(lambda t: all(f(t) for f in [folder_filter, mov_filter]), fileList)
             _fileList = filter(lambda file: os.path.splitext(file)[1] == '.jpg', fileList)
-            fildDict = {k: pylev.levenschtein(k, mov) for k in _fileList }
-            _fileList = sorted(fildDict, key=fildDict.__getitem__)
+
+            if _fileList == [] :
+                continue
+            fileDict = {k: pylev.levenschtein(k, mov) for k in _fileList }
+            _fileList = sorted(fileDict, key=fileDict.__getitem__)
             self.lv.Items.Clear()
             self.loc.Text = dirName
             map(lambda x: self.lv.Items.Add(x) , _fileList)    
-            for fname in _fileList:
-                finished = yield '%s/%s' % (dirName, fname)
-                if finished:
-                    break
+            finished = yield '%s/%s' % (dirName, _fileList[0])
 
     def setImage(self, path):
         self.txt.Text = path
@@ -50,27 +50,33 @@ class MyWindow(Window):
             self.img.Source = image
         except:
             pass
-               
-    def Next_Click(self, sender, e): # next button
-        path = self.itor.send(False)
-        self.setImage(path)
-        
+
     def OK_Click(self, sender, e): # ok button
-        path = self.itor.send(True)
-        oldpath = self.txt.Text
-        self.setImage(path)
-        #self.sts.Text = oldpath
-        try:
-            os.rename(oldpath, os.path.join(os.path.dirname(oldpath), 'folder.jpg'))
+        try:       
+            oldpath = self.txt.Text
+            os.rename(self.selectFile, os.path.join(os.path.dirname(self.selectFile), 'folder.jpg'))
         except:
             pass
 
-    def Skip_Click(self, sender, e): # ok button
-        path = self.itor.send(True)
+        try:
+            path = self.itor.send(True)
+            self.setImage(path)
+        except StopIteration:
+            Application.Current.Shutdown()
+
+
+    def Skip_Click(self, sender, e): # skip button
+        try:
+            path = self.itor.send(True)
+        except StopIteration:
+            Application.Current.Shutdown()
         self.setImage(path)
     
     def lv_SelectionChanged(self, sender, e):
-        pass
-
+        if sender.SelectedItem != None:
+            self.sts.Text = "item selected : " + sender.SelectedItem
+            self.selectFile = self.loc.Text + "/" + sender.SelectedItem
+            self.setImage(self.selectFile)
+    
 if __name__ == '__main__':
     Application().Run(MyWindow())
